25aa82282a02ef4881de5fce786a71cde8b1d3b2
RodrigoFrancaBR <rfrancacosta@gmail.com>
2021-02-16 22:47:07 -0200

    Hello World Spring Boot

---
+++ forum/src/main/java/br/com/alura/forum/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+	@ResponseBody
+	@RequestMapping
+	public String helloWorld() {
+		return "Hello World!";
+	}
+}
