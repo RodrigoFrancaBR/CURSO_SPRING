e9d3e2715e712b84deb60fe8b223c41938795b6d
RodrigoFrancaBR <rfrancacosta@gmail.com>
2021-03-11 16:41:23 -0300

    Configurando autenticação Stateless

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -30,9 +31,13 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll();
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/auth").permitAll()
 		.anyRequest().authenticated()
-		.and().formLogin();
+		.and().csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		// cria sessão e um form de login
+		// .and().formLogin();
 	}
 	
 	// serve para fazermos configurações de recursos estáticos. São requisições para arquivo CSS, Javascript, imagens, etc. 
